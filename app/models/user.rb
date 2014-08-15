f45477bb5d308f1a5054790f8fcae25404e74bd8
Garrett Carey <garretticarey@gmail.com>
2014-08-15 00:59:03 -0400

    Google Sign In integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauth_providers => [:google_oauth2]
 
   # Validation
   validates :first_name, presence: true
@@ -36,5 +36,21 @@ class User < ActiveRecord::Base
   # Display
   def full_name
     [first_name, last_name[0.1]].join(" ")+"."
+  end
+
+
+  # Google+ with OmniAuth
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+     unless user
+         user = User.create(first_name: data["name.givenName"],
+            last_name: data["name.familyName"],
+            email: data["email"],
+            password: Devise.friendly_token[0,20]
+         )
+     end
+    user
   end
 end
