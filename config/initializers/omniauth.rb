4e5238a66ceb2233520d852b67ccb4d6b0156296
Garrett Carey <garretticarey@gmail.com>
2014-08-15 02:06:17 -0400

    OmniAuth added to Devise initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "37721039452-oi2gb2kequ6ljqtfs80u61dmthkfaffk.apps.googleusercontent.com", ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+    {
+      :image_aspect_ratio => "square",
+      :image_size => 190
+    }
 end
