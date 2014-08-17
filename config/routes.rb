dea9fc973b802f5a69c71f89a26cca3c15973314
Garrett Carey <garretticarey@gmail.com>
2014-08-17 15:30:44 -0400

    Fixed OC route default

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   get 'users/:id' => 'users#show', as: :profile
   get 'inbox' => 'inbox#index'
-  match "/contacts/gmail/callback" => "friends#callback"
+  get 'contacts/gmail/callback' => 'friends#callback'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
