a6067ca7896d5717dc51eb446b5dc36d18f4a7cd
Garrett Carey <garretticarey@gmail.com>
2014-08-17 14:45:57 -0400

    Trying controller side gen of findfriends page

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,7 +3,11 @@ class FriendsController < ApplicationController
   # GET /tips
   # GET /tips.json
   def callback
-	  @contacts = request.env['omnicontacts.contacts']
-	  @user = current_user
+	@contacts = request.env['omnicontacts.contacts']
+  @user = request.env['omnicontacts.user']
+  puts "List of contacts of #{user[:name]} obtained from #{params[:importer]}:"
+  @contacts.each do |contact|
+    puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
+  end
   end
 end
