c9321dc011434499ea584a02abac7c7e3eac978e
Garrett Carey <garretticarey@gmail.com>
2014-08-17 14:16:07 -0400

    First attempt at generating Find Friends page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   get 'users/:id' => 'users#show', as: :profile
   get 'inbox' => 'inbox#index'
+  get 'findfriends' => 'friends#callback'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
