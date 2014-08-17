ec56515019de1e448f447986074c4e6db262826f
Garrett Carey <garretticarey@gmail.com>
2014-08-17 14:29:14 -0400

    Removed false SSL cert from OmniContacts controller

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,5 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {:redirect_path => "/findfriends", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
+  importer :gmail, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {:redirect_path => "/findfriends"}
 end
