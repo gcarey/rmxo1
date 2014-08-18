0ec447bd72f3261e51c81ff48d417612a854d811
Garrett Carey <garretticarey@gmail.com>
2014-08-18 16:36:14 -0400

    More OA tweaks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in user, :event => :authentication
-        @after_sign_in_url = after_sign_in_path_for(user)
+        sign_in @user, :event => :authentication
+        @after_sign_in_url = after_sign_in_path_for(@user)
         render 'pages/callback', :layout => false
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
