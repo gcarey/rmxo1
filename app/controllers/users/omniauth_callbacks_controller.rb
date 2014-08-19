0306f7b96be687ef5c8a69fb7b289334547010ca
Garrett Carey <garretticarey@gmail.com>
2014-08-19 16:10:09 -0400

    OA for javascript disabled

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         sign_in @user, :event => :authentication
         @after_sign_in_url = after_sign_in_path_for(@user)
-        render 'pages/callback', :layout => false
+        if request.env['omniauth.params']['popup']
+          render 'pages/callback', :layout => false
+        else
+          redirect_to @after_sign_in_url
+        end
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
         flash.notice = "Dang it! Something went wrong."
