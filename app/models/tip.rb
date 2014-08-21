9fbc876a3c1c2cbee1151d26a806198d1d148190
Garrett Carey <garretticarey@gmail.com>
2014-08-20 20:24:46 -0400

    Better fix for false image Grabbit bug

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -13,9 +13,9 @@ class Tip < ActiveRecord::Base
 
   # Scrape images, title, description
   def scrape_with_grabbit
-    # I highly recommend passing the following call off to a Resque worker, or Delayed Job queue.
-    # The reason is that Grabbit will attempt to access the remote URL. If there is a network problem,
-    # or the remote URL is unavailable, the following line could hang up your Rails process.
+    ### I highly recommend passing the following call off to a Resque worker, or Delayed Job queue.
+    ### The reason is that Grabbit will attempt to access the remote URL. If there is a network problem,
+    ### or the remote URL is unavailable, the following line could hang up your Rails process.
 
     data = Grabbit.url(link)
 
@@ -27,28 +27,22 @@ class Tip < ActiveRecord::Base
         $offset = 0
         # Run until an image is saved
         while self.image_file_name == nil
-          # Check if image exists by looking for HTTP success code
+          # Attempt to extract file dimensions
           begin
-            url = URI.parse(data.images.drop($offset).first)
-            req = Net::HTTP.new(url.host, url.port)
-            res = req.request_head(url.path) 
+            geometry = Paperclip::Geometry.from_file(data.images.drop($offset).first)
           rescue
-            # If no code at all, return false instead of erroring out
             false
           else
-            # Run only if success code
-            if req.request_head(url.path).code == "200"
-              # Check and conditionally set file with offset of 0, 1, 2, etc.
-              geometry = Paperclip::Geometry.from_file(data.images.drop($offset).first)
-              if geometry.width.to_i >= 225 && geometry.height.to_i >= 225
-                self.image = url
-              end
+            # Set file as image if dimensions fit
+            if geometry.width.to_i >= 225 && geometry.height.to_i >= 225
+              self.image = url
             end
           end
           # Move on to next image
           $offset +=1
-          # If no more images to check, stop checking ("use first image" commented out)
+          # If no more images to check, stop checking
           if data.images.drop($offset).first == nil
+            ## And use first image
             #self.image = URI.parse(data.images.first)
             break
           end
