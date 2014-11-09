bbd4ee7fac0986780a74ecbb5dfa2f5fb5789edc
Garrett Carey <garretticarey@gmail.com>
2014-11-09 15:23:59 -0500

    Basic mailer setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 
 	def create
 	  @user = User.create(user_params)
+    Notifications.welcome(@user).deliver
 	end
 
 
---
+++ app/mailers/notifications.rb
@@ -0,0 +1,10 @@
+class Notifications < ActionMailer::Base
+  default from: "notifications@tipster.to"
+
+  def welcome(user)
+    @user = user
+    @url  = 'http://www.tipster.to/users/sign_in'
+		attachments.inline['logo.png'] = File.read('/assets/logo.png')
+    mail(to: @user.email, subject: 'Tipster — Thanks for signing up!')
+  end
+end
