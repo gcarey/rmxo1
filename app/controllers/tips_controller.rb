5d6db9d917909258064bfa9c4b00e45a21ccf4eb
Garrett Carey <garretticarey@gmail.com>
2014-11-12 12:05:48 -0500

    Pushed back tip email delivery

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -19,9 +19,6 @@ class TipsController < ApplicationController
     @recipient = User.find(params[:recipient_id])
     @received_tip.recipients << @recipient
 
-    @share = @tip.shares.where(user_id: @recipient.id).last
-    Notifications.tip(@recipient, @share, current_user).deliver
-
     #Check if the tip being saved is a reshare
     if current_user.received_tips.where(link: params[:tip][:link]).last != nil
       current_user.received_tips.where(link: params[:tip][:link]).each do |t|
@@ -37,6 +34,8 @@ class TipsController < ApplicationController
 
     respond_to do |format|
       if @tip.valid? && @tip.save
+        @share = @tip.shares.where(user_id: @recipient.id).last
+        Notifications.tip(@recipient, @share, current_user).deliver
         format.html { redirect_to root_url, notice: 'Tip sent!' }
       elsif !@tip.valid?
         format.html { redirect_to root_url, notice: "That doesn't seem to be a real URL. Did you include http:// at the beginning?" }
