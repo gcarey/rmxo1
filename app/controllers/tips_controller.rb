1168555ca8d9ec8298fe9b69ed13239e85cc6318
Garrett Carey <garretticarey@gmail.com>
2014-11-12 11:36:39 -0500

    Pluralized share call in tips controller

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -17,7 +17,10 @@ class TipsController < ApplicationController
     end
     @received_tip = @tip
     @recipient = User.find(params[:recipient_id])
-    @received_tip.recipients << @recipient    
+    @received_tip.recipients << @recipient
+
+    @share = @tip.shares.where(user_id: @recipient.id).last
+    Notifications.tip(@recipient, @share, current_user).deliver
 
     #Check if the tip being saved is a reshare
     if current_user.received_tips.where(link: params[:tip][:link]).last != nil
@@ -34,8 +37,6 @@ class TipsController < ApplicationController
 
     respond_to do |format|
       if @tip.valid? && @tip.save
-        @share = @tip.share.where(user_id: @recipient.id).last
-        Notifications.tip(@recipient, @share, current_user).deliver
         format.html { redirect_to root_url, notice: 'Tip sent!' }
       elsif !@tip.valid?
         format.html { redirect_to root_url, notice: "That doesn't seem to be a real URL. Did you include http:// at the beginning?" }
