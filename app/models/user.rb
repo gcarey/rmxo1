009fa83b1825a2deff422ede92c1b6081e8fbba4
Garrett Carey <garretticarey@gmail.com>
2014-12-05 21:13:30 -0500

    Mobile spacer fix, omniauth grab avatar fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,14 +55,16 @@ class User < ActiveRecord::Base
     data = access_token.info
     user = User.where(:email => data["email"]).first
 
-     unless user
-         user = User.create(first_name: data["first_name"],
-            last_name: data["last_name"],
-            email: data["email"],
-            password: Devise.friendly_token[0,20],
-            avatar: data["picture"]
-         )
-     end
+    unless user
+      user = User.create(
+        first_name: data["first_name"],
+        last_name: data["last_name"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20],
+        avatar: data["image"]
+        provider: "google"
+        )
+    end
     user
   end
 
