455362bb04b0b7409cd12d5af6be15498b16b4e7
Garrett Carey <garretticarey@gmail.com>
2014-12-07 03:14:04 -0500

    Email tip link error fix

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -3,9 +3,13 @@
   def visit_link
     tip = Tip.find(params[:id])
     redirect_to "http://redirect.viglink.com?key=2936e5116a8b6c8d103ef904f2da99fd&u="+CGI.escape(tip.link)
-    shares = tip.shares.where(user_id: current_user.id)
-    shares.update_all(visited: true)
+    if params[:sender]
+      shares = tip.shares.where(user_id: params[:sender])
+    else
+      shares = tip.shares.where(user_id: current_user.id)
     end
+    shares.update_all(visited: true)
+  end
 
   def destroy
     tip = Tip.find(params[:id])
