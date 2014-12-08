a3431f9626da9016a94ba36fd5e7d1a04cb42b4c
Garrett Carey <garretticarey@gmail.com>
2014-12-08 14:29:32 -0500

    Remove devise admin resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208175122) do
-
-  create_table "admins", force: true do |t|
-    t.string   "email",               default: "", null: false
-    t.string   "encrypted_password",  default: "", null: false
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+ActiveRecord::Schema.define(version: 20141208173916) do
 
   create_table "friendships", force: true do |t|
     t.integer  "user_id"
