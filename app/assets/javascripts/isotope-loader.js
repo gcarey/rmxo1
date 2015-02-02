e38f2020e624e5e57c4164d2303a453cfc51e739
Garrett Carey <garretticarey@gmail.com>
2015-02-01 20:36:53 -0500

    Hi res Google Reg buttons for mobile

--- app/assets/javascripts/isotope-loader.js
+++ app/assets/javascripts/isotope-loader.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 	// Isotope Loader
 	var $container = $('#page');
 
-	if ( $(window).width() > 768) {     
+	if ( $(window).width() >= 768) {     
 		$container.isotope({itemSelector: '.tip', layoutMode: 'masonryHorizontal', stamp: '#stamp' });
 	} else {
 		$container.isotope({itemSelector: '.tip', layoutMode: 'masonry', stamp: '#stamp' });
 	}
 
 	$( window ).resize(function() {
-	  if ( $(window).width() > 768) { 
+	  if ( $(window).width() >= 768) { 
 	  	$container.isotope('destroy')
 			$container.isotope({itemSelector: '.tip', layoutMode: 'masonryHorizontal', stamp: '#stamp' });
 		} else {
