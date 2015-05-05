1f69c3cd2cf03d0ca2240ccae70e4c4e8492743b
Garrett Carey <garretticarey@gmail.com>
2015-05-05 15:05:04 -0400

    Remove viglink redirect

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,7 +2,8 @@
 
   def visit_link
     tip = Tip.find(params[:id])
-    redirect_to "http://redirect.viglink.com?key=2936e5116a8b6c8d103ef904f2da99fd&u="+CGI.escape(tip.link)
+    # redirect_to "http://redirect.viglink.com?key=2936e5116a8b6c8d103ef904f2da99fd&u="+CGI.escape(tip.link)
+    redirect_to tip.link
     if params[:invitee]
       shares = tip.outgoing_shares.where(invitee_id: params[:invitee])
     elsif params[:recipient]
