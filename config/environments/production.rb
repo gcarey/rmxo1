d0500a4ff18e2e64202cb180c30a3c15f8b1c577
Garrett Carey <garretticarey@gmail.com>
2016-05-10 11:08:49 -0400

    Sendgrid mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,13 +95,15 @@ Rails.application.configure do
 
   # Mailer
   ActionMailer::Base.smtp_settings = {
-      :port =>           '587',
-      :address =>        'smtp.mandrillapp.com',
-      :user_name =>      ENV['MANDRILL_USERNAME'],
-      :password =>       ENV['MANDRILL_APIKEY'],
-      :domain =>         'heroku.com',
-      :authentication => :plain
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
+
   ActionMailer::Base.delivery_method = :smtp
 
 end
